29e53e3d6617917ebdbc6412d1943dd726670714
DFalque <falque@me.com>
2021-01-14 18:09:39 +0100

    corrección video

--- src/components/Frame3.js
+++ src/components/Frame3.js
@@ -7,7 +7,7 @@ export class Frame3 extends Component {
         className="video__embed"
         width="560"
         height="315"
-        src="https://www.youtube.com/watch?v=8b2c8_lHgoQ"
+        src="https://www.youtube.com/embed/8b2c8_lHgoQ"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
